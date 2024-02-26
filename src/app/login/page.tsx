dc4d105214e32ca8941ecdd94d53d4a3ad1345cf
DenisKantic <denis.kantic18@gmail.com>
2024-02-26 23:06:17 +0100

    finished login design with responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
+import Image from 'next/image'
 
 const Login = () => {
   return (
-    <div>
-        <h1>Login</h1>
+    <div 
+    className='w-full p-10 bg-[#FEF1FD] min-h-screen flex justify-center items-center overflow-hidden
+    sm:flex-col lg:flex-row'>
+
+    <div 
+    className='flex w-[50%] h-[50vh] 
+               sm:h-[30vh] lg:hidden'>
+        <Image 
+        src="/images/zenskaBG.png"
+        alt='zenska vizija logo'
+        width={50}
+        height={50}
+        className='w-full h-full object-contain'
+        />
+    </div>
+
+    <form
+    className='mx-auto h-[50vh] flex flex-col overflow-hidden
+    sm:w-[90%] lg:w-[50%] sm:p-0 md:p-10'>
+        <h1 
+        className='text-4xl text-[#AC009B] font-bold p-3'>
+        Dobrodošli!
+        </h1>
+
+        <input 
+        type="email" 
+        required 
+        placeholder='Unesite Vaš mail' 
+        className='w-full mt-10 py-3 p-10 text-[#C86DD7] text-xl rounded-full outline-none cursor-pointer
+        hover:outline-1 hover:outline-[#F93EDF] focus:outline-[#AC009B]'
+        /> 
+
+        <br />
+
+        <input 
+        type="password" 
+        required 
+        placeholder='Šifra' 
+        className='w-full mt-5 py-3 p-10 text-[#C86DD7] text-xl rounded-full outline-none cursor-pointer
+        hover:outline-1 hover:outline-[#F93EDF] focus:outline-[#AC009B]'
+        /> 
+
+        <br />
+
+        <a 
+        href="#"
+        className='text-lg text-[#C86DD7] m-3 italic underline'
+        >Zaboravili ste šifru?</a>
+
+        <div className='w-full mt-5 p-3 flex flex-row justify-between items-center'>
+            <button
+            className='w-[45%] bg-none border border-[2px] border-[#F93EDF] rounded-full py-3 text-lg text-[#F93EDF]
+                       hover:bg-[#F93EDF] hover:text-white hover:font-bold'
+            >
+            Napusti
+            </button>
+            <button
+            className='w-[45%] bg-[#F93EDF] border border-[2px] border-[#F93EDF] rounded-full py-3 text-lg
+                       hover:bg-transparent hover:border-[#F93EDF] hover:font-bold hover:text-[#F93EDF]'
+            >
+            Prijavi se
+            </button>
+        </div>
+
+    </form>
+
+    <div className='flex p-10 w-[50%] h-[50vh] sm:hidden lg:block'>
+        <Image 
+        src="/images/zenskaBG.png"
+        alt='zenska vizija logo'
+        width={50}
+        height={50}
+        className='w-full h-full object-contain'
+        />
+    </div>
+
     </div>
   )
 }
