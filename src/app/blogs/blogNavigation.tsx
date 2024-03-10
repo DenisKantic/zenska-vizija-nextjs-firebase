5e3a11a916b8361b1905e4307d6076b7348b3e50
Bego96 <begic.ismar96@gmail.com>
2024-03-11 00:07:04 +0100

    Finnished blogs routes with dummy data

--- src/app/blogs/blogNavigation.tsx
+++ src/app/blogs/blogNavigation.tsx
@@ -1,28 +1,25 @@
 'use client'
-import React, { useState } from 'react'
-import { blogData } from '../home/blog/data/dummyData'
+import React from 'react';
+import { blogData } from '../home/blog/data/dummyData';
 import Link from 'next/link';
+import { usePathname } from "next/navigation";
+
 export default function BlogNavigation() {
-  const [activeCol, setActiveCol] = useState();
+  const pathname = usePathname();
   const blogs = blogData;
 
-  const findBlog = blogData.find(blog => blog.id === 1);
-  
-  const settingActiveCol = (value: any) => {
-    setActiveCol(value)
-  }
   return (
     <div className=''>
-      <h3 className='text-slate-700 text-2xl font-semibold mt-16 mb-4 px-6'>Istaknuti članci</h3>
+      <h3 className='text-slate-700 text-2xl font-semibold mt-16 mb-8 px-6'>Istaknuti članci</h3>
       {
         blogs.slice(0, 5).map(blog => (
           <Link key={blog.id} href={`/blogs/${blog.id}`}>
-          <div onClick={() => settingActiveCol(blog.id)} className={`${activeCol === blog.id ? 'bg-[#F93EDF] text-white' : '' } hover:bg-[#F93EDF] cursor-pointer p-6 rounded-xl`}>
-            <p>{blog.title}</p>
-          </div>
+            <div className={`${pathname === `/blogs/${blog.id}` ? "bg-[#F93EDF] text-white" : ""} hover:bg-[#F93EDF] hover:text-white cursor-pointer px-6 py-4 rounded-xl mb-2`}>
+              <p>{blog.title}</p>
+            </div>
           </Link>
         ))
       }
     </div>
-  )
+  );
 }
