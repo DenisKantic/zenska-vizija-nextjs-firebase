08141e9619a970411774dc0f72836fa6d784eb39
Bego96 <begic.ismar96@gmail.com>
2024-03-31 20:19:28 +0200

    Fixes to responsive

--- src/app/home/events/Events.tsx
+++ src/app/home/events/Events.tsx
@@ -57,37 +57,37 @@ const Events = () => {
         id="events"
         className="flex items-center justify-center font-bold py-4 pt-5 lg:px-6 mt-12 text-chineseBlack"
       >
-        <h1 className="lg:text-5xl sm:text-[22px] xs:text-[24px] opacity-80">
+        <h1 className="lg:text-5xl sm:text-[22px] xxs:text-[24px] opacity-80">
           Skorašnji Događaji
         </h1>
       </div>
       <div
-        className="flex items-center justify-center text-chineseBlack opacity-80 xs:text-[12px]
-      lg:text-xl md:text-[15px] sm:text-[14px] lg:pt-5 lg:pb-4 lg:px-6 lg:my-3 sm:px-9 sm:py-3 xs:px-6 xs:py-4"
+        className="flex items-center justify-center text-chineseBlack opacity-80 xxs:text-[12px]
+      lg:text-xl md:text-[15px] sm:text-[14px] lg:pt-5 lg:pb-4 lg:px-6 lg:my-3 sm:px-9 sm:py-3 xxs:px-6 xxs:py-4"
       >
-        <p className="xs:text-center">
+        <p className="xxs:text-center">
           Vrlo često organizujemo događaje na razne teme, jer brinemo o našim
           članicama
         </p>
       </div>
       <div
-        className="flex lg:mt-4 flex-wrap lg:justify-end md:justify-center sm:justify-end items-center xs:justify-center 
-      mr-4 mb-2 lg:py-4 lg:px-5 sm:pr-2 sm:mt-5 xs:my-4 md:pl-4"
+        className="flex lg:mt-4 flex-wrap lg:justify-end md:justify-center sm:justify-end items-center xxs:justify-center 
+      mr-4 mb-2 lg:py-4 lg:px-5 sm:pr-2 sm:mt-5 xxs:my-4 md:pl-4"
       >
         <span
           className="text-chineseBlack lg:text-xl sm:text-[14px] lg:mr-4 lg:pt-2 lg:pr-4 
-        md:mr-2 md:pr-2 md:mb-2 xs:text-[12px] xs:pb-1 xs:px-2"
+        md:mr-2 md:pr-2 md:mb-2 xxs:text-[12px] xxs:pb-1 xxs:px-2"
         >
           Vidi starije događaje
         </span>
         <div
-          className="flex flex-wrap lg:space-x-2 xs:space-x-2 sm:space-x-1 md:mb-2
-        lg:mr-8 sm:pr-2 sm:pt-1 xs:ml-3"
+          className="flex flex-wrap lg:space-x-2 xxs:space-x-2 sm:space-x-1 md:mb-2
+        lg:mr-8 sm:pr-2 sm:pt-1 xxs:ml-3"
         >
-          <div className="lg:px-2 xs:pr-2">
+          <div className="lg:px-2 xxs:pr-2">
             <Image
               className="hover:text-purplePizzazz lg:h-full lg:w-full md:h-[80%] md:w-[80%] 
-              sm:h-[80%] sm:w-[80%] xs:h-[80%] xs:w-[90%] cursor-pointer"
+              sm:h-[80%] sm:w-[80%] xxs:h-[80%] xxs:w-[90%] cursor-pointer"
               src="/images/landing/grayArrow.png"
               alt="grayArrow"
               width={100}
@@ -98,7 +98,7 @@ const Events = () => {
           <div className="lg:px-3">
             <Image
               className=" hover:text-grayy lg:h-full lg:w-full md:h-[80%] md:w-[80%] 
-              sm:h-[80%] sm:w-[80%] xs:h-[80%] xs:w-[90%] cursor-pointer"
+              sm:h-[80%] sm:w-[80%] xxs:h-[80%] xxs:w-[90%] cursor-pointer"
               src="/images/landing/pinkArrow.png"
               alt="pinkArrow"
               width={100}
@@ -109,8 +109,8 @@ const Events = () => {
         </div>
       </div>
       <div
-        className="flex lg:flex-row lg:justify-between xs:justify-center xs:flex-col md:justify-center sm:flex-col 
-        md:flex-col lg:py-4 xs:px-3 xs:py-3 xs:gap-3 md:gap-2 lg:px-6"
+        className="flex lg:flex-row lg:justify-between xxs:justify-center xxs:flex-col md:justify-center sm:flex-col 
+        md:flex-col lg:py-4 xxs:px-3 xxs:py-3 xxs:gap-3 md:gap-2 lg:px-6"
       >
         {currentEvents.map((event: any) => (
           <EventCard
