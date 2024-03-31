12d546b529e5e0ed20fc2633cea7a17fd82bf9ed
Filip <filipsarovic428@gmail.com>
2024-03-31 18:03:19 +0200

    responsive home

--- src/app/home/navigation/Navbar.tsx
+++ src/app/home/navigation/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
 
   return (
     <div
-      className="fixed lg:w-[92%] lg:py-2 lg:left-3 shadow-xl bg-[#fff] mt-2 md:mr-12 sm:left-3
-        lg:h-[75px] md:h-[50px] sm:h-[42px] xs:w-[98%] xxs:w-[98%] rounded-[16px] top-0 z-10 right-1 lg:ml-10"
+      className="fixed lg:w-[92%] lg:left-3 shadow-xl bg-[#fff] mt-2 md:mr-12 sm:left-3 xs:mr-2
+        lg:h-[13%] md:h-[10%] sm:h-[42px] xs:w-[95%] xxs:w-[98%] rounded-[16px] top-0 z-10 right-1 lg:ml-10"
     >
       <div className="lg:hidden md:hidden sm:hidden xs:absolute xs:right-3 xxs:absolute xxs:right-3">
         {showNav ? (
@@ -39,7 +39,7 @@ const Navbar = () => {
         )}
       </div>
       <div
-        className="flex lg:justify-between xs:flex-col xs:items-start 
+        className="flex lg:justify-between xs:flex-col xs:items-start
         lg:flex-row md:flex-row sm:flex-row items-center lg:h-full lg:w-full"
       >
         <div className="flex xs:items-start lg:ml-5 md:ml-4 sm:ml-3">
@@ -49,134 +49,142 @@ const Navbar = () => {
               alt="logo"
               width={110}
               height={110}
-              className="px-3 lg:my-auto md:mt-1 sm:mt-0 xs:pt-1 cursor-pointer 
+              className="px-3 lg:py-3 md:mt-1 sm:mt-0 xs:pt-1 cursor-pointer 
               lg:w-[110px] md:w-20 sm:w-[75px]"
               priority
             />
           </Link>
         </div>
         {/* start of the nav list */}
-        <ul
-          className={`lg:flex md:flex sm:flex xs:items-start lg:mx-auto md:mx-auto md:mr-2 
-          sm:mx-auto sm:mr-4 lg:mr-2 lg:gap-1 sm:gap-2 ${
+        <div className="lg:my-auto lg:px-2 md:my-auto">
+          <ul
+            className={`lg:flex md:flex sm:flex xs:items-start lg:mx-auto md:ml-5 
+          sm:mx-auto sm:mr-4 lg:mr-2 lg:gap-2 sm:gap-2 xs:space-y-2 xs:py-3 ${
             showNav ? "" : "hidden"
           }`}
-        >
-          {/* about-us */}
-          <li
-            className="flex rounded-[16px] hover:bg-crayola hover:text-lotion
-           lg:px-4 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
           >
-            <Image
-              src="/images/landing/information-circle.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#about-us"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] whitespace-nowrap sm:py-1 sm:px-2"
+            {/* about-us */}
+            <li
+              className="flex rounded-[16px] lg:hover:bg-crayola lg:hover:text-white
+           lg:px-2 md:py-1 md:px-2 xs:space-x-2 xs:px-4 md:hover:bg-crayola"
             >
-              O nama
-            </a>
-          </li>
-          {/* programs */}
-          <li
-            className="flex hover:bg-crayola hover:text-white rounded-[16px]
-           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
-          >
-            <Image
-              src="/images/landing/programs_icon.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#programs"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] sm:py-1 sm:px-2"
+              <Image
+                src="/images/landing/information-circle.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#about-us"
+                className="lg:text-[16px] md:text-[12px] lg:hover:text-white xs:hover:text-crayola
+              sm:text-[10px] whitespace-nowrap sm:py-1 sm:px-2 lg:py-2 lg:my-2"
+              >
+                O nama
+              </a>
+            </li>
+            {/* programs */}
+            <li
+              className="flex lg:hover:bg-crayola lg:hover:text-white rounded-[16px]
+           lg:px-2 md:px-2 xs:space-x-2 xs:px-4 md:hover:bg-crayola"
             >
-              Programi
-            </a>
-          </li>
-          {/* events */}
-          <li
-            className="flex hover:bg-crayola hover:text-white rounded-[16px]
-           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
-          >
-            <Image
-              src="/images/landing/events_icon.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#events"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] sm:py-1 sm:px-2"
+              <Image
+                src="/images/landing/programs_icon.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#programs"
+                className="lg:text-[16px] md:text-[12px] sm:text-[10px] md:py-1 lg:py-2
+              sm:px-2 lg:hover:text-white xs:hover:text-crayola md:hover:text-white"
+              >
+                Programi
+              </a>
+            </li>
+            {/* events */}
+            <li
+              className="flex lg:hover:bg-crayola lg:hover:text-white rounded-[16px]
+           lg:px-5 md:px-2 xs:space-x-2 xs:px-4 md:hover:bg-crayola"
             >
-              Događaji
-            </a>
-          </li>
-          {/* blog */}
-          <li
-            className="flex hover:bg-crayola hover:text-white rounded-[16px]
-           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
-          >
-            <Image
-              src="/images/landing/blog_icon.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#blogs"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] sm:py-1 sm:px-2"
+              <Image
+                src="/images/landing/events_icon.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#events"
+                className="lg:text-[16px] md:text-[12px] sm:text-[10px] md:hover:text-white
+              sm:px-2 lg:py-3 lg:hover:text-white xs:hover:text-crayola"
+              >
+                Događaji
+              </a>
+            </li>
+            {/* blog */}
+            <li
+              className="flex lg:hover:bg-crayola lg:hover:text-white rounded-[16px]
+           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1 md:hover:bg-crayola"
             >
-              Blog
-            </a>
-          </li>
-          {/* gallery */}
-          <li
-            className="flex hover:bg-crayola hover:text-white rounded-[16px]
-           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
-          >
-            <Image
-              src="/images/landing/gallery_icon.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#gallery"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] sm:py-1 sm:px-2"
+              <Image
+                src="/images/landing/blog_icon.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#blogs"
+                className="lg:text-[16px] md:text-[12px] sm:text-[10px] md:hover:text-white
+              sm:py-1 sm:px-2 lg:hover:text-white xs:hover:text-crayola"
+              >
+                Blog
+              </a>
+            </li>
+            {/* gallery */}
+            <li
+              className="flex lg:hover:bg-crayola lg:hover:text-white rounded-[16px]
+           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1 md:hover:bg-crayola"
             >
-              Galerija
-            </a>
-          </li>
-          {/* contact */}
-          <li
-            className="flex hover:bg-crayola hover:text-white rounded-[16px]
-           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1"
-          >
-            <Image
-              src="/images/landing/contact_icon.png"
-              alt="home"
-              className="w-6 h-6 lg:hidden md:hidden sm:hidden"
-              width={20}
-              height={20}
-            />
-            <a
-              href="#contact"
-              className="lg:text-[16px] md:text-[12px] sm:text-[10px] sm:py-1 sm:px-2"
+              <Image
+                src="/images/landing/gallery_icon.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#gallery"
+                className="lg:text-[16px] md:text-[12px] sm:text-[10px] md:hover:text-white
+              sm:py-1 sm:px-2 lg:hover:text-white xs:hover:text-crayola"
+              >
+                Galerija
+              </a>
+            </li>
+            {/* contact */}
+            <li
+              className="flex lg:hover:bg-crayola lg:hover:text-white rounded-[16px]
+           lg:px-5 lg:py-2 md:py-1 md:px-2 sm:mt-2 lg:mt-1 md:hover:bg-crayola"
             >
-              Kontaktiraj nas
-            </a>
-          </li>
-        </ul>
+              <Image
+                src="/images/landing/contact_icon.png"
+                alt="home"
+                className="w-6 h-6 lg:hidden md:hidden sm:hidden"
+                width={20}
+                height={20}
+              />
+              <a
+                href="#contact"
+                className="lg:text-[16px] md:text-[12px] sm:text-[10px] md:hover:text-white
+              sm:py-1 sm:px-2 lg:hover:text-white xs:hover:text-crayola"
+              >
+                Kontaktiraj nas
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
